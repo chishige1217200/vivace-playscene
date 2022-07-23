c8f1277a9df8a138bb084cc600ba021e563ae291
chishige1217200 <daichicchi01@gmail.com>
2022-07-23 21:49:34 +0900

    Update: 長押しノーツの途中点用判定追加

--- Assets/Scripts/PlayScene/LineJudger.cs
+++ Assets/Scripts/PlayScene/LineJudger.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 public class LineJudger : MonoBehaviour
 {
     [SerializeField] int lineNum = 0;
-
+    public bool isTouched { get; private set; } = false;
     void Start() // 自オブジェクトのEvent Triggerに関数のコール情報を自動追加します
     {
         //Fetch the Event Trigger component from your GameObject
@@ -34,7 +34,7 @@ public class LineJudger : MonoBehaviour
         //Add a PointerDown type event to the Event Trigger
         entry.eventID = EventTriggerType.PointerDown;
         //call the PointerDown function when the Event System detects dragging
-        entry.callback.AddListener((data) => { PointerDown(); });
+        entry.callback.AddListener((data) => { PointerDown((PointerEventData)data); });
         //Add the trigger entry
         trigger.triggers.Add(entry);
 
@@ -47,11 +47,29 @@ public class LineJudger : MonoBehaviour
         //Add the trigger entry
         trigger.triggers.Add(entry);
 
+        //Create a new entry for the Event Trigger
+        entry = new EventTrigger.Entry();
+        //Add a PointerUp type event to the Event Trigger
+        entry.eventID = EventTriggerType.PointerUp;
+        //call the PointerUp function when the Event System detects dragging
+        entry.callback.AddListener((data) => { PointerUp((PointerEventData)data); });
+        //Add the trigger entry
+        trigger.triggers.Add(entry);
+
+        //Create a new entry for the Event Trigger
+        entry = new EventTrigger.Entry();
+        //Add a PointerExit type event to the Event Trigger
+        entry.eventID = EventTriggerType.PointerExit;
+        //call the PointerExit function when the Event System detects dragging
+        entry.callback.AddListener((data) => { PointerExit(); });
+        //Add the trigger entry
+        trigger.triggers.Add(entry);
+
     }
     public void BeginDrag(PointerEventData data)
     {
         //Debug.Log("BeginDrag: " + lineNum + "," + data.position.y + "," + data.pointerId);
-        CoordYPresever.AddCoordY(data.position.y, lineNum);
+        //CoordYPresever.AddCoordY(data.position.y, lineNum);
     }
 
     public void Drop(PointerEventData data)
@@ -67,17 +85,41 @@ public class LineJudger : MonoBehaviour
         {
             PlaySceneProcessManager.JudgeTiming(lineNum, 2);
         }
+        isTouched = false;
     }
 
-    public void PointerDown()
+    public void PointerDown(PointerEventData data)
     {
         //Debug.Log("PointerDown: " + lineNum);
         PlaySceneProcessManager.JudgeTiming(lineNum, 1);
+        CoordYPresever.AddCoordY(data.position.y, lineNum);
+        isTouched = true;
+    }
+
+    public void PointerUp(PointerEventData data)
+    {
+        int result = CoordYPresever.isFlick(data.position.y, lineNum);
+        if (result == 1) // フリック
+        {
+            Debug.Log("Flick Up!");
+            PlaySceneProcessManager.JudgeTiming(lineNum, 5);
+        }
+        else // 長押し離す
+        {
+            PlaySceneProcessManager.JudgeTiming(lineNum, 2);
+        }
+        isTouched = false;
     }
 
     public void PointerEnter(PointerEventData data)
     {
         //Debug.Log("PointerEnter: " + lineNum + "," + data.position.y + "," + data.pointerId);
         CoordYPresever.AddCoordY(data.position.y, lineNum);
+        isTouched = true;
+    }
+
+    public void PointerExit()
+    {
+        isTouched = false;
     }
 }
